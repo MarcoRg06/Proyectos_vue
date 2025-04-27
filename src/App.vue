2a4d75f04907838a520c0eb57dd885fdbf57f82e
MarcoRg06 <22620119@tlaxiaco.tecnm.mx>
2025-04-26 20:51:23 -0600

    Agregando el diseño de la barra de navegacion Navbar

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,37 @@
 
 <template>
     <div id="app">
-    <h1>Vue Router</h1>
-    <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-   </nav>
-    <router-view></router-view> 
-</div> 
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link to="/">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/about">About</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <!-- <nav>
+   </nav> -->
+        <router-view></router-view>
+    </div>
 </template>
 
 <style scoped>
